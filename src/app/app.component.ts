184a459f7201fa34189e494cb1d7c1e6cf449a29
911joeblack <74622790+911joeblack@users.noreply.github.com>
2022-08-20 23:49:11 -0700

    Hello World Prototype

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:
+  `
+    <div><h1>{{ pageTitle }}</h1></div>
+  `
 })
+
 export class AppComponent {
-  title = 'ToDoList';
+  pageTitle = 'To Do List';
 }
